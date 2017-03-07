6fefe93bcbe6ff1b8cd9ce4b217e128946b5d73d
Robert Marcinkowski <robertm1506@gmail.com>
2017-03-07 14:41:52 +0100

    site template

---
+++ js/script.js
@@ -0,0 +1,21 @@
+
+
+$(window).on('load', function () {
+    $('.preloader').addClass('invisible');
+    $('.main-effect').addClass('visible');
+});
+
+$(window).scroll(function () {
+    var scroll = $(window).scrollTop() + 600;
+    $('.block').each(function () {
+        var element = $(this).offset().top;
+        if (scroll > element) {
+            $(this).addClass('visible');
+        }
+    });
+    var top = $(document).scrollTop();
+    $('.main').css({
+        'background-position': 'center ' + (top / 2).toFixed(2) + 'px'
+    });
+
+});
